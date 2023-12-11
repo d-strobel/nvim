4d9e62abc4c1cd1aa47dec2a71026bd4997c5249
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-12-11 22:29:24 +0100

    feat: add keymaps

--- lua/dstrobel/core/remaps.lua
+++ lua/dstrobel/core/remaps.lua
@@ -5,3 +5,13 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Centralize while going page up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Void registry pasting 
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Copy to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
