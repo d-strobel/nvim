ecbd6f93f93efb511c7bd27faad3ef4a188166ef
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-12-11 21:49:47 +0100

    feat: add language server

--- lua/dstrobel/lazy.lua
+++ lua/dstrobel/lazy.lua
@@ -37,5 +37,13 @@ require("lazy").setup({
     {
     'tpope/vim-fugitive', name = 'fugitive'
     },
+    --- LSP
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 })
 
