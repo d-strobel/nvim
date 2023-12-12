a7c7c294f77d5e1c7a98cdf15cdc81b413969204
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-12-12 14:50:33 +0100

    feat: add personal keymaps for nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,8 +1,30 @@
+local api = require("nvim-tree.api")
+
+-- Keymaps
+vim.keymap.set('n', '<leader>pv', ":NvimTreeToggle<CR>")
+vim.keymap.set('n', '<leader>pf', ":NvimTreeFocus<CR>")
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- Keymappings on attach
+local function on_attach(bufnr)
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set("n", "l", api.node.open.edit, opts "Open")
+  vim.keymap.set("n", "<CR>", api.node.open.edit, opts "Open")
+  vim.keymap.set("n", "r", api.fs.rename_sub, opts "Rename")
+  vim.keymap.set("n", "q", api.tree.close, opts "Close")
+  vim.keymap.set("n", "p", api.fs.paste, opts "Paste file")
+  vim.keymap.set("n", "y", api.fs.copy.node, opts "Copy file")
+  vim.keymap.set("n", "d", api.fs.remove, opts "Delete file")
+end
+
 require("nvim-tree").setup({
+  on_attach = on_attach,
   sort = {
     sorter = "case_sensitive",
   },
@@ -24,6 +46,7 @@ require("nvim-tree").setup({
     },
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
+    custom = { "^.git$" }
   },
 })
--- lua/dstrobel/core/remaps.lua
+++ lua/dstrobel/core/remaps.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Move multilines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
