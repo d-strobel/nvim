51f97da9fb61a47b581399e102ba6c5d9b581a75
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-02-21 10:35:17 +0100

    feat: add some keymaps

--- lua/dstrobel/core/keymaps.lua
+++ lua/dstrobel/core/keymaps.lua
@@ -3,3 +3,21 @@ vim.g.mapleader = " "
 
 -- for conciseness
 local keymap = vim.keymap
+
+-- Move multilines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Centralize while going page up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Void registry pasting
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Copy to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Delete to void registry
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
