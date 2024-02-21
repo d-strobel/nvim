b1bfdc52fe678d25033444926d2059862be1d7aa
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-02-21 10:42:26 +0100

    feat: add vim options

--- lua/dstrobel/core/options.lua
+++ lua/dstrobel/core/options.lua
@@ -1,1 +1,18 @@
--- Options here
+-- Line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Indenting
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Searching
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Scrollin
+vim.opt.scrolloff = 8
+
