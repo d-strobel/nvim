ab04309922a2ad442a869659ddfa4670459ff8b8
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-02-23 00:41:08 +0100

    feat: modify keymaps

--- lua/dstrobel/core/keymaps.lua
+++ lua/dstrobel/core/keymaps.lua
@@ -5,19 +5,23 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 -- Move multilines in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Centralize while going page up and down
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Void registry pasting
-vim.keymap.set("x", "<leader>p", [["_dP]])
+keymap.set("x", "<leader>p", [["_dP]])
 
 -- Copy to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Paste form system clipboard
+keymap.set({ "n", "v" }, "<leader>p", [["+p]])
+keymap.set("n", "<leader>P", [["+P]])
 
 -- Delete to void registry
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+keymap.set({ "n", "v" }, "<leader>d", [["_d]])
