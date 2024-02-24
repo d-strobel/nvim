237c73debe44fc52e5ff43bb26a9328053d4411e
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-02-24 12:19:14 +0100

    feat: change keymaps for fugitive

--- lua/dstrobel/plugins/fugitive.lua
+++ lua/dstrobel/plugins/fugitive.lua
@@ -3,10 +3,10 @@ return {
   config = function()
     local api = vim.api
 
-    api.nvim_set_keymap("n", "<leader>gs", ":Git<CR>", { noremap = true, silent = true })
-    -- api.nvim_set_keymap("n", "<leader>gc", ":Git commit<CR>", { noremap = true, silent = true })
-    -- api.nvim_set_keymap("n", "<leader>gp", ":Git push<CR>", { noremap = true, silent = true })
-    -- api.nvim_set_keymap("n", "<leader>gd", ":Gdiff<CR>", { noremap = true, silent = true })
-    -- api.nvim_set_keymap("n", "<leader>gl", ":Glog<CR>", { noremap = true, silent = true })
+    api.nvim_set_keymap("n", "<leader>gs", "<CMD>Git<CR>", { noremap = true, silent = true })
+    api.nvim_set_keymap("n", "<leader>gc", "<CMD>Git commit<CR>", { noremap = true, silent = true })
+    api.nvim_set_keymap("n", "<leader>gp", "<CMD>Git push<CR>", { noremap = true, silent = true })
+    api.nvim_set_keymap("n", "<leader>gd", "<CMD>Git diff<CR>", { noremap = true, silent = true })
+    api.nvim_set_keymap("n", "<leader>gl", "<CMD>Git log<CR>", { noremap = true, silent = true })
   end
 }
