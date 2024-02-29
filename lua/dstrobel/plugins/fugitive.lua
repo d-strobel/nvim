1ece8f4281c3254d0487bfcd3878246681c6171c
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-02-29 15:07:09 +0100

    feat: add keymap for git commit

--- lua/dstrobel/plugins/fugitive.lua
+++ lua/dstrobel/plugins/fugitive.lua
@@ -1,12 +1,24 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    local api = vim.api
 
-    api.nvim_set_keymap("n", "<leader>gs", "<CMD>Git<CR>", { noremap = true, silent = true })
-    api.nvim_set_keymap("n", "<leader>gc", "<CMD>Git commit<CR>", { noremap = true, silent = true })
-    api.nvim_set_keymap("n", "<leader>gp", "<CMD>Git push<CR>", { noremap = true, silent = true })
-    api.nvim_set_keymap("n", "<leader>gd", "<CMD>Git diff<CR>", { noremap = true, silent = true })
-    api.nvim_set_keymap("n", "<leader>gl", "<CMD>Git log<CR>", { noremap = true, silent = true })
+    -- Git status
+    vim.keymap.set("n", "<leader>gs", "<CMD>Git<CR>", { noremap = true, silent = true })
+
+    -- Commit with message
+    vim.keymap.set("n", "<leader>gc", function()
+      local msg = vim.fn.input("Commit message: ")
+      -- Concat message string
+      if msg ~= "" then
+        msg = "Git commit -m '" .. msg
+        vim.cmd(msg .. "'")
+      else
+        vim.cmd("Git commit")
+      end
+    end, { noremap = true, silent = true })
+
+    vim.keymap.set("n", "<leader>gp", "<CMD>Git push<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>gd", "<CMD>Git diff<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>gl", "<CMD>Git log<CR>", { noremap = true, silent = true })
   end
 }
