45043554357de1798760258f58d40ca33a72cd73
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-03-02 14:22:25 +0100

    feat: update lspconfig with borders on hover

--- lua/dstrobel/plugins/lsp/lspconfig.lua
+++ lua/dstrobel/plugins/lsp/lspconfig.lua
@@ -5,20 +5,21 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
   },
+
   config = function()
-    -- import lspconfig plugin
     local lspconfig = require("lspconfig")
-
-    -- import cmp-nvim-lsp plugin
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local windows = require('lspconfig.ui.windows')
 
+    -- used for keymaps
     local keymap = vim.keymap
-
     local opts = { noremap = true, silent = true }
-    local on_attach = function(client, bufnr)
+
+    -- on attach keymaps
+    local on_attach = function(_, bufnr)
       opts.buffer = bufnr
 
-      -- Keymaps
+      -- Set keymaps
       opts.desc = "Show LSP references"
       keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
@@ -43,24 +44,33 @@ return {
       opts.desc = "Show line diagnostics"
       keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
       opts.desc = "Show documentation for what is under cursor"
       keymap.set({"n", "i"}, "<C-h>", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
     end
+
+    -- set the border for floating windows
+    windows.default_options.border = 'single'
+
+    vim.lsp.handlers["textDocument/hover"] =
+    vim.lsp.with(
+      vim.lsp.handlers.hover,
+      {
+        border = "single"
+      }
+    )
+
+    vim.lsp.handlers["textDocument/signatureHelp"] =
+    vim.lsp.with(
+      vim.lsp.handlers.signature_help,
+      {
+        border = "single"
+      }
+    )
 
     -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
@@ -77,6 +87,13 @@ return {
     lspconfig["terraformls"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
+
+      vim.api.nvim_create_autocmd({"BufWritePre"}, {
+        pattern = { ".tf", ".tfvars" },
+        callback = function()
+          vim.lsp.buf.format()
+        end
+      }),
     })
 
     -- configure html server
