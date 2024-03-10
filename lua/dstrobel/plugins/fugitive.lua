57b65dafb9bed34a280116981b2248d760367636
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-03-10 17:53:30 +0100

    feat: change keymaps for fugitive

--- lua/dstrobel/plugins/fugitive.lua
+++ lua/dstrobel/plugins/fugitive.lua
@@ -1,24 +1,15 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-
     -- Git status
     vim.keymap.set("n", "<leader>gs", "<CMD>Git<CR>", { noremap = true, silent = true })
 
-    -- Commit with message
-    vim.keymap.set("n", "<leader>gc", function()
-      local msg = vim.fn.input("Commit message: ")
-      -- Concat message string
-      if msg ~= "" then
-        msg = "Git commit -m '" .. msg
-        vim.cmd(msg .. "'")
-      else
-        vim.cmd("Git commit")
-      end
-    end, { noremap = true, silent = true })
-
+    -- Usefull keymaps
+    vim.keymap.set("n", "<leader>gc", "<CMD>Git commit", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>gp", "<CMD>Git push<CR>", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>gd", "<CMD>Git diff<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>gD", "<CMD>Telescope git_status<CR>", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>gl", "<CMD>Git log<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>gL", "<CMD>Telescope git_commits<CR>", { noremap = true, silent = true })
   end
 }
