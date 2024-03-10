edb264532623fa078361e2d19e333dcb6249e683
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-03-10 17:59:01 +0100

    feat: add keymap for git commit

--- lua/dstrobel/plugins/fugitive.lua
+++ lua/dstrobel/plugins/fugitive.lua
@@ -4,8 +4,19 @@ return {
     -- Git status
     vim.keymap.set("n", "<leader>gs", "<CMD>Git<CR>", { noremap = true, silent = true })
 
-    -- Usefull keymaps
-    vim.keymap.set("n", "<leader>gc", "<CMD>Git commit", { noremap = true, silent = true })
+    -- Git commit with message input
+    vim.keymap.set("n", "<leader>gc", function()
+      local msg = vim.fn.input("Commit message > ")
+      -- Concat message string
+      if msg ~= "" then
+        msg = "Git commit -m '" .. msg
+        vim.cmd(msg .. "'")
+      else
+        vim.cmd("Git commit")
+      end
+    end, { noremap = true, silent = true })
+
+    -- Other usefull keymaps
     vim.keymap.set("n", "<leader>gp", "<CMD>Git push<CR>", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>gd", "<CMD>Git diff<CR>", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>gD", "<CMD>Telescope git_status<CR>", { noremap = true, silent = true })
