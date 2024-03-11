6f7a37865904842f73db13e800e312984305618f
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-03-11 21:30:28 +0100

    feat: add harpoon number switch

--- lua/dstrobel/plugins/harpoon.lua
+++ lua/dstrobel/plugins/harpoon.lua
@@ -24,6 +24,7 @@ return {
     keymap.set("n", "<A-2>", function() harpoon:list():select(2) end, { desc = "Go to harpoon file 2" })
     keymap.set("n", "<A-3>", function() harpoon:list():select(3) end, { desc = "Go to harpoon file 3" })
     keymap.set("n", "<A-4>", function() harpoon:list():select(4) end, { desc = "Go to harpoon file 4" })
+    keymap.set("n", "<A-5>", function() harpoon:list():select(5) end, { desc = "Go to harpoon file 5" })
   end,
 
 }
