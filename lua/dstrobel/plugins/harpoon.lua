ca7d831ef928b510fb37e2b7f3409d1c7d387383
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-03-11 21:26:54 +0100

    feat: switch back to harpoon ui

--- lua/dstrobel/plugins/harpoon.lua
+++ lua/dstrobel/plugins/harpoon.lua
@@ -6,31 +6,17 @@ return {
   },
   config = function()
     local harpoon = require('harpoon')
+
+    -- Required
     harpoon:setup({})
-
-    -- basic telescope configuration
-    local conf = require("telescope.config").values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-          results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-      }):find()
-    end
 
     -- set keymaps
     local keymap = vim.keymap
-    keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon ui with telescope" })
+    -- keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon ui with telescope" })
+    keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     keymap.set("n", "<A-a>", function() harpoon:list():append() end, { desc = "Add file to harpoon" })
+    keymap.set("n", "<A-d>", function() harpoon:list().vim:append() end, { desc = "Add file to harpoon" })
     keymap.set("n", "<A-k>", function() harpoon:list():next() end, { desc = "Go to next harpoon file" })
     keymap.set("n", "<A-j>", function() harpoon:list():prev() end, { desc = "Go to previous harpoon file" })
 
