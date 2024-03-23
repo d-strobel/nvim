a72a99ad94318febaa26eacd0c2adec658fbef33
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-03-23 23:04:22 +0100

    fix: cmp cursed preview overlay

--- lua/dstrobel/plugins/nvim-cmp.lua
+++ lua/dstrobel/plugins/nvim-cmp.lua
@@ -19,7 +19,7 @@ return {
 
     cmp.setup({
       completion = {
-        completeopt = "menu,menuone,preview,noselect",
+        completeopt = "menu,menuone,noselect",
       },
       snippet = { -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
