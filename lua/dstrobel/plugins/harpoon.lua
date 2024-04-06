40e92b52fc5d0873abb1f25203292170c77d1e7a
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-04-06 02:02:39 +0200

    feat: add harpoon shortcuts

--- lua/dstrobel/plugins/harpoon.lua
+++ lua/dstrobel/plugins/harpoon.lua
@@ -15,8 +15,7 @@ return {
     -- keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon ui with telescope" })
     keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    keymap.set("n", "<A-a>", function() harpoon:list():append() end, { desc = "Add file to harpoon" })
-    keymap.set("n", "<A-d>", function() harpoon:list().vim:append() end, { desc = "Add file to harpoon" })
+    keymap.set("n", "<A-a>", function() harpoon:list():add() end, { desc = "Add file to harpoon" })
     keymap.set("n", "<A-k>", function() harpoon:list():next() end, { desc = "Go to next harpoon file" })
     keymap.set("n", "<A-j>", function() harpoon:list():prev() end, { desc = "Go to previous harpoon file" })
 
@@ -25,6 +24,21 @@ return {
     keymap.set("n", "<A-3>", function() harpoon:list():select(3) end, { desc = "Go to harpoon file 3" })
     keymap.set("n", "<A-4>", function() harpoon:list():select(4) end, { desc = "Go to harpoon file 4" })
     keymap.set("n", "<A-5>", function() harpoon:list():select(5) end, { desc = "Go to harpoon file 5" })
-  end,
 
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-l>", function()
+          harpoon.ui:select_menu_item()
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set("n", "<C-v>", function()
+          harpoon.ui:select_menu_item({ vsplit = true })
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set("n", "<C-x>", function()
+          harpoon.ui:select_menu_item({ split = true })
+        end, { buffer = cx.bufnr })
+      end,
+    })
+  end,
 }
