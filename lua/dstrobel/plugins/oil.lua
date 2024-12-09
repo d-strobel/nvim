0258db7f7c7acc981ace0b6ee890e2933b33cf42
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-12-09 20:16:30 +0100

    feat: replace netrw with oil

---
+++ lua/dstrobel/plugins/oil.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    config = function()
+      require("oil").setup({
+        use_default_keymaps = false,
+        keymaps = {
+          ["<CR>"] = "actions.select",
+          ["<C-l>"] = "actions.select",
+          ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+          ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+          ["<Esc>"] = { "actions.close", mode = "n" },
+          ["<leader>e"] = { "actions.parent", mode = "n" },
+        },
+        view_options = {
+          show_hidden = true,
+        },
+        float = {
+          -- Padding around the floating window
+          padding = 6,
+          max_width = 0,
+          max_height = 0,
+          border = "rounded",
+          win_options = {
+            winblend = 0,
+          },
+        },
+      })
+
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "<space>e", require("oil").toggle_float, { desc = "Open parent directory in floating window" })
+    end,
+  },
+}
