c94b8fac9f817acb6cd5d3d271929836101861ee
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-12-15 17:48:48 +0100

    feat: switch to catppuccin color theme

--- lua/dstrobel/lazy.lua
+++ lua/dstrobel/lazy.lua
@@ -13,7 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ import = "dstrobel.plugins" }, {
   install = {
-    colorscheme = { "carbonfox" },
+    colorscheme = { "catppuccin" },
   },
   checker = {
     enabled = true,
--- lua/dstrobel/plugins/colors.lua
+++ lua/dstrobel/plugins/colors.lua
@@ -1,54 +1,60 @@
 return {
   {
-    "EdenEast/nightfox.nvim",
-    name = "nightfox",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
     config = function()
-      require('nightfox').setup({
-        options = {
-          -- Compiled file's destination location
-          compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-          compile_file_suffix = "_compiled", -- Compiled file suffix
-          transparent = true,                -- Disable setting background
-          terminal_colors = true,            -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-          dim_inactive = false,              -- Non focused panes set to alternative background
-          module_default = true,             -- Default enable value for modules
-          colorblind = {
-            enable = false,                  -- Enable colorblind support
-            simulate_only = false,           -- Only show simulated colorblind colors and not diff shifted
-            severity = {
-              protan = 0,                    -- Severity [0,1] for protan (red)
-              deutan = 0,                    -- Severity [0,1] for deutan (green)
-              tritan = 0,                    -- Severity [0,1] for tritan (blue)
-            },
-          },
-          styles = {           -- Style to be applied to different syntax groups
-            comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
-            conditionals = "NONE",
-            constants = "NONE",
-            functions = "NONE",
-            keywords = "NONE",
-            numbers = "NONE",
-            operators = "NONE",
-            strings = "NONE",
-            types = "NONE",
-            variables = "NONE",
-          },
-          inverse = { -- Inverse highlight for different types
-            match_paren = false,
-            visual = false,
-            search = false,
-          },
-          modules = {}, -- List of various plugins and additional options
+      require("catppuccin").setup({
+        flavour = "mocha",
+        background = {
+          light = "latte",
+          dark = "mocha",
         },
-        palettes = {},
-        specs = {},
-        groups = {},
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = true,     -- shows the '~' characters after the end of buffers
+        term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false,             -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15,           -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = true,              -- Force no italic
+        no_bold = false,               -- Force no bold
+        no_underline = false,          -- Force no underline
+        styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = {},
+          conditionals = {},
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = false,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
       })
 
       -- load the colorscheme here
-      vim.cmd [[colorscheme carbonfox]]
+      vim.cmd [[colorscheme catppuccin]]
       vim.cmd [[highlight NormalFloat guibg=NONE]]
     end
   }
